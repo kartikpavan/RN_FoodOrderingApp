5c430bdb686264a943443da282d99c0c31dc0a59
Pavan <kartik.varanasi@yahoo.com>
2024-02-09 13:18:37 +0530

    supabase setup

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+import * as SecureStore from "expo-secure-store";
+
+const ExpoSecureStoreAdapter = {
+  getItem: async (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://ohqueqltlhjiayacqkip.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ocXVlcWx0bGhqaWF5YWNxa2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NjQ0NjksImV4cCI6MjAyMzA0MDQ2OX0.9q9wzvmuWJdpYFJacA7o76L7KWi5Y4f5o9oEZeyLBn8";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
